4ea9847f6fad9f85ca2c17e72bdb605cb4a92278
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-12-06 00:27:01 +0800

    Optimize Tenon.Infra.Swagger (#159)

---
+++ src/Infrastructures/Tenon.Infra.Swagger/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Tenon.Infra.Swagger.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSwaggerBearerAuthorizationHeader(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(o =>
+        {
+            o.CustomSchemaIds(id => id.FullName!.Replace('+', '-'));
+
+            var securityScheme = new OpenApiSecurityScheme
+            {
+                Name = "JWT Authentication",
+                Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\"",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                BearerFormat = "JWT"
+            };
+
+            o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
+
+            var securityRequirement = new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = JwtBearerDefaults.AuthenticationScheme
+                        }
+                    },
+                    []
+                }
+            };
+
+            o.AddSecurityRequirement(securityRequirement);
+        });
+
+        return services;
+    }
+}
